fc1bc75999784526532fc345c84d36053b09b3d3
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-04-21 10:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function rot13(str) {
-  // Split the input string into an array of characters.
-  return str.split('')
-    // For each character in the array:
-    .map(function(char) {
-      // Convert char to its ASCII code.
-      var code = char.charCodeAt(0);
-      // If the character is not an uppercase letter (ASCII codes 65-90), leave it as it is.
-      if (code < 65 || code > 90) {
-        return char;
-      }
-      // Otherwise, shift the letter 13 places forward or backward in the alphabet,
-      // depending on whether its ASCII code is below or above 77 ('M').
-      if (code < 78) {
-        code += 13;
-      } else {
-        code -= 13;
-      }
-      // Convert the new ASCII code back to a character.
-      return String.fromCharCode(code);
-    })
-    // Join the array of characters back into a string.
-    .join('');
+function fot13(str) {
+	let nice = [];
+	for(let i=0; i<str.length; i++){
+		if(str[i]>='A' && str[i]<='M'){
+			nice.push(String.fromCharCode(str.charCodeAt(i)+13));
+		}else if(str[i]>='N'&&str[i]<='Z'){
+			nice.push(String.fromCharCode(str.charCodeAt(i)-13));
+		}else{
+			nice.push(str[i]);
+		}
+	}
+    let ans = nice.join("");
+    return ans;
 }
